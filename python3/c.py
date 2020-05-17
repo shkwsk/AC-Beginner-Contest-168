46f65014f4cce528147fa68038d1b0903ae672e5
shkwsk <ren7link@gmail.com>
2020-05-17 23:41:26 +0900

    fix c

--- python3/c.py
+++ python3/c.py
@@ -5,11 +5,6 @@ def main():
     degH = (0.5*H*60 + 0.5*M)
     degM = 6*M
     theta = abs(degH-degM)
-    if theta == 180:
-        print(A+B)
-        return
-    if theta > 180:
-        theta = 180 - (theta - 180)
     print(math.sqrt(A**2 + B**2 - 2*A*B*math.cos(math.radians(theta))))
 
 if __name__ == '__main__':
